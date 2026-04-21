6b390ab0bb2cca7fe8506c247dffdc1a76a03a97
Bohdan Kulbaka <bkulbaka1990@gmail.com>
2026-04-21 17:16:43 +0300

    make alien animation

--- alien.py
+++ alien.py
@@ -6,11 +6,13 @@ class Alien(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.images = [
-        get_frame(SPRITE_SHEET, 4, 20, 8, 7),
-        get_frame(SPRITE_SHEET, 34, 52, 13, 9),
+        get_frame(SPRITE_SHEET, 4, 20, 8, 8),#первый пришелец
+        get_frame(SPRITE_SHEET, 20, 20, 8, 8),
+        get_frame(SPRITE_SHEET, 34, 52, 13, 9),#спрайт взрыва
         get_frame(SPRITE_SHEET, 33, 67, 15, 11)
         ]
         self.images = [pygame.transform.scale(img, (32, 32)) for img in self.images]
+        self.first_alien_animation = self.images[0:2]
         self.image = self.images[0]
         self.rect = self.image.get_frect()
         self.rect.center = (x, y)
@@ -18,13 +20,14 @@ class Alien(pygame.sprite.Sprite):
         self.move_timer = 0
         self.death_status = False
         self.death_timer = 0
+        self.frame = 0
 
 
     def update(self, dt, direction):
         self.new_movement(dt, direction)
         if self.death_status:
             self.death(dt)
-            print(self.death_timer)
+
 
     def new_movement(self, dt, direction):
         self.move_timer += dt
@@ -32,7 +35,10 @@ class Alien(pygame.sprite.Sprite):
         if self.move_timer < 0.3:
             self.rect.centerx += self.speed * dt * direction
 
+
         if self.move_timer >= 1:
+            self.frame = (self.frame + 1) % len(self.first_alien_animation)
+            self.image = self.first_alien_animation[self.frame]
             self.move_timer = 0
 
 
@@ -42,6 +48,6 @@ class Alien(pygame.sprite.Sprite):
         if self.death_timer > 0.6:
             self.kill()
         if self.death_timer < 0.6:
-            self.image = self.images[2]
+            self.image = self.images[3]
         if self.death_timer < 0.3:
-            self.image = self.images[1]
+            self.image = self.images[2]
