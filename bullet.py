d1931be945eccc2e8643e5631506b2a9c8c3b6a5
Bohdan Kulbaka <bkulbaka1990@gmail.com>
2026-04-22 16:01:47 +0300

    done with bullets

--- bullet.py
+++ bullet.py
@@ -5,8 +5,15 @@ from settings import *
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, owner):
         pygame.sprite.Sprite.__init__(self)
-        self.image = get_frame(SPRITE_SHEET, 39, 5, 1, 6)
-        self.image = pygame.transform.scale(self.image, (3, 20))
+        self.images =[
+            get_frame(SPRITE_SHEET, 39, 5, 1, 6),
+            get_frame(SPRITE_SHEET, 38, 21, 3, 7)
+        ]
+        if owner == "player":
+            self.images = [pygame.transform.scale (img, (5,20)) for img in self.images]
+        if owner == "alien":
+            self.images = [pygame.transform.scale(img, (15, 15)) for img in self.images]
+        self.image = self.images[0]
         self.rect = self.image.get_frect()
         self.rect.center = (x, y)
         self.owner = owner
@@ -17,6 +24,7 @@ class Bullet(pygame.sprite.Sprite):
             self.rect.centery -= 5
             self.death()
         elif self.owner == "alien":
+            self.image = self.images[1]
             self.rect.centery += 3
             self.death()
 
