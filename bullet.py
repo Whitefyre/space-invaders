d646788a8747319cc60676146d7f192c229d6cd9
Bohdan Kulbaka <bkulbaka1990@gmail.com>
2026-04-22 11:01:35 +0300

    make new player bullet

--- bullet.py
+++ bullet.py
@@ -1,11 +1,12 @@
 import pygame
+from utils import get_frame
+from settings import SPRITE_SHEET
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('bullet.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (100,5))
-        self.image = pygame.transform.rotozoom(self.image, 90, 1)
+        self.image = get_frame(SPRITE_SHEET, 39, 5, 1, 6)
+        self.image = pygame.transform.scale(self.image, (3, 20))
         self.rect = self.image.get_frect()
         self.rect.center = (x, y)
 
